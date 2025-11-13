a3356d36d92ebe8c26f77ea5c713a872f665ac25
DavidMRB <davidrangelbaez@gmail.com>
2025-11-12 20:34:41 -0500

    Fourth Push

--- cypress/tests/ui/custom-flow.spec.ts
+++ cypress/tests/ui/custom-flow.spec.ts
@@ -6,11 +6,11 @@ describe("Flujo Completo de Usuario", () => {
   const transactionPage = new TransactionPage();
 
   // Datos de prueba
-    const testUser = {
+  const testUser = {
     username: "Katharina_Bernier",
     // 🔒 Evitamos hard-coded passwords:
     password: Cypress.env("TEST_USER_PASSWORD") || "default_password",
-    };
+  };
 
   beforeEach(() => {
     // Limpiar cookies y visitar la app
